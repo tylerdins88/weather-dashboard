514d24cb6991f0334bd8d1d0f1548f4d9ee787bc
Tyler <tyler.dins88@gmail.com>
2023-01-18 15:03:35 -0600

    initial styling and index building

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,19 @@
-var weather = $("#displayWeather")
-var fetchBtn = $("#fetchBtn")
+var weather = $("#displayWeather");
+var fetchBtn = $("#fetchBtn");
 
 function getWeatherAPI() {
-    console.log("Hello")
-    var weatherAPI = "http://api.openweathermap.org/geo/1.0/direct?q=Denver&limit=3&appid=e655f88c2e522bfcf96e8b9280a63f61"
+    var weatherLoc = $("#weatherLoc").val();
+    console.log(weatherLoc)
+    // var weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?q=" + weatherLoc + "&c3f6b2733fb2d47bd01e52a022b99a72"
+    // var weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?zip=" + weatherLoc + "&appid=e655f88c2e522bfcf96e8b9280a63f61"
+    var weatherAPI = "http://api.openweathermap.org/geo/1.0/direct?q=" + weatherLoc + "&limit=5&appid=e655f88c2e522bfcf96e8b9280a63f61"
 
     fetch(weatherAPI)
-    console.log("Hello")
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
-            weather.textContent = data;
         })
 }
 
