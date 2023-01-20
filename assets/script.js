689568f8380d4b62154af367cfeaba6ef5a16a9d
Tyler <tyler.dins88@gmail.com>
2023-01-20 13:04:22 -0600

    created buttons for list locations

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,83 @@
 var currentWeather = $("#currentWeather");
 var fetchBtn = $("#fetchBtn");
 var prevLocations = $("#prevLocations")
-
+var enteredLoc = "";
+var listMain = "";
+var weatherData;
+// current format of timeNow is in milliseconds since epoch
+var timeNow = dayjs().unix();
+// var iconUrl = `https://openweathermap.org/img/w/${** your - data - ojb **.icon}.png`;
 
 function getWeatherAPI(event) {
     event.preventDefault();
     var weatherLoc = $("#weatherLoc").val();
-    console.log(weatherLoc)
+    console.log(weatherLoc);
     // var weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?q=" + weatherLoc + "&appid=c3f6b2733fb2d47bd01e52a022b99a72"
-    var weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?zip=" + weatherLoc + "&appid=e655f88c2e522bfcf96e8b9280a63f61"
+    var weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?zip=" + weatherLoc + "&units=imperial&appid=e655f88c2e522bfcf96e8b9280a63f61"
     // var weatherAPI = "http://api.openweathermap.org/geo/1.0/direct?q=" + weatherLoc + "&limit=5&appid=c3f6b2733fb2d47bd01e52a022b99a72"
 
-    var enteredLoc = "";
     fetch(weatherAPI)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
             console.log(data);
-            enteredLoc = data.city.name
-            console.log(enteredLoc)
+            weatherData = data;
             addLoc();
+            showCurrentWeather();
         })
 
+    // function listFetch(event) {
+    //     event.preventDefault();
+    //     var listLoc = // need this to be the button value
+
+    //     var listAPI = "http://api.openweathermap.org/data/2.5/forecast?zip=" + listLoc + "&units=imperial&appid=e655f88c2e522bfcf96e8b9280a63f61"
+
+    //     fetch(listAPI)
+    //         .then(function (response) {
+    //             return response.json();
+    //         })
+    //         .then(function (data) {
+    //             console.log(data);
+    //             weatherData = data;
+    //             showCurrentWeather()
+    //         })
+    // }
 
     function addLoc() {
-        console.log("Hello")
-        var lastLocation = document.createElement("button")
-        lastLocation.setAttribute("type", "submit")
-        lastLocation.setAttribute("class", "lastLocation")
-        lastLocation.textContent = enteredLoc
-        prevLocations.append(lastLocation)
+        enteredLoc = weatherData.city.name;
+        console.log(enteredLoc);
+        var lastListed = document.createElement("li");
+        var lastLocation = document.createElement("input");
+        lastLocation.setAttribute("type", "submit");
+        lastLocation.setAttribute("class", "btn btn-primary");
+        lastLocation.setAttribute("value", enteredLoc);
+        lastLocation.addEventListener("click", getWeatherAPI)
+        lastListed.append(lastLocation);
+        prevLocations.append(lastListed);
+        // i need to make this button link back to the listed api
+    }
+
+    function showCurrentWeather() {
+        // i need to find the date, tempature, wind, humidity for 
+        enteredLoc = weatherData.city.name;
+        var currentTemp = weatherData.list[0].main.temp + " Degrees F";
+        var currentSky = weatherData.list[0].weather[0].description;
+        var currentWind = weatherData.list[0].wind.speed + " MPH";
+        var currentHumidity = weatherData.list[0].main.humidity + " Percent";
+        console.log(enteredLoc, currentTemp, currentSky, currentWind, currentHumidity);
+        currentWeather.text("The current weather for " + enteredLoc + " at " + dayjs().format("dddd, MMMM D, YYYY") + " is as follows:");
+        currentWeather.append(currentTemp);
+        currentWeather.append(currentSky);
+        currentWeather.append(currentWind);
+        currentWeather.append(currentHumidity);
+
     }
 }
 
 fetchBtn.on("click", getWeatherAPI);
+
+// need to link dayJS
 
 // when page opens. have local storage location displayed.
 // if local storage empty, have random weather location displayed. 
